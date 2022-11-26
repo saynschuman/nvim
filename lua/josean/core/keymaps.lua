82dffb8e5a4127acce661db3fb4796bed3f7375e
saynschuman <saynschuman@gmail.com>
2022-11-26 18:49:38 +0200

    file browser added

--- lua/josean/core/keymaps.lua
+++ lua/josean/core/keymaps.lua
@@ -54,6 +54,7 @@ keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git 
 keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
 keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
 keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+keymap.set("n", "<space>tb", ":Telescope file_browser<cr>", { noremap = true })
 
 -- telescope open recent
 vim.api.nvim_set_keymap(
--- lua/josean/plugins/telescope.lua
+++ lua/josean/plugins/telescope.lua
@@ -23,18 +23,6 @@ telescope.setup({
     },
     file_ignore_patterns = {
       "^frontend/node_modules/",
-      "%.sh",
-      "%.html",
-      "%.css",
-      "%.js",
-      "%.yml",
-      "%.ssl",
-      "%.MD",
-      "%.lock",
-      "^backend/",
-      "%.py",
-      "%.todo",
-      "%.txt",
     },
   },
 })
@@ -43,3 +31,26 @@ telescope.load_extension("fzf")
 
 -- recent files extension
 require("telescope").load_extension("recent_files")
+-- You don't need to set any of these options.
+
+-- IMPORTANT!: this is only a showcase of how you can set default options!
+require("telescope").setup({
+  extensions = {
+    file_browser = {
+      theme = "ivy",
+      -- disables netrw and use telescope-file-browser in its place
+      hijack_netrw = true,
+      mappings = {
+        ["i"] = {
+          -- your custom insert mode mappings
+        },
+        ["n"] = {
+          -- your custom normal mode mappings
+        },
+      },
+    },
+  },
+})
+-- To get telescope-file-browser loaded and working with telescope,
+-- you need to call load_extension, somewhere after setup function:
+require("telescope").load_extension("file_browser")
