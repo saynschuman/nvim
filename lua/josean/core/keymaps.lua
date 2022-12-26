5877576f06f76745dfb0cc44fc230d0feddb06d1
saynschuman <saynschuman@gmail.com>
2022-12-27 00:12:40 +0200

    livegrep added

--- lua/josean/core/keymaps.lua
+++ lua/josean/core/keymaps.lua
@@ -2,6 +2,7 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
+local builtin = require("telescope.builtin")
 
 ---------------------
 -- General Keymaps
@@ -48,6 +49,7 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
 -- telescope git commands (not on youtube nvim video)
 keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
